ef6c0b04e0c9d259099cc1586f6e9d4377171016
Perriesha <pmbyrd@gmail.com>
2023-08-20 20:45:44 -0700

    Added fullName property

--- models/customer.js
+++ models/customer.js
@@ -59,6 +59,11 @@ class Customer {
     return await Reservation.getReservationsForCustomer(this.id);
   }
 
+  /** get the full name of the customer. */
+  // NOTE: this is a "getter property" that is accessed like an attribute
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
   /** save this customer. */
 
   async save() {
